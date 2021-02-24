9d0c4f78d5ce6b37866250b17b6fe692b59ab97a
Raul Resendiz <rresendizlazaro@hotmail.com>
2021-02-23 18:39:37 -0600

    Task2

---
+++ src/test/java/mx/edu/j2se/resendiz/tasks/tests/ArrayTaskList.java
@@ -0,0 +1,32 @@
+package mx.edu.j2se.resendiz.tasks.tests;
+
+import mx.edu.j2se.resendiz.tasks.Task;
+
+public class ArrayTaskList {
+    //Attributes
+    Task arrayTaskList[];
+
+    //Methods
+    public void add(Task task){
+
+        this.arrayTaskList[0] = task;
+    }
+
+    public boolean remove(Task task){
+        for (int i = 0; i <= this.arrayTaskList.length; i++){
+            if (arrayTaskList.equals(task)){
+                arrayTaskList[i] = null;
+                break;
+            }
+        }
+        return true;
+    }
+
+    public int size(){
+        return this.arrayTaskList.length;
+    }
+
+    public Task getTask(int index){
+        return this.arrayTaskList[index];
+    }
+}
