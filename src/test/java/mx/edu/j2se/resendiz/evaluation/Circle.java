ecd45bb4e38bdc3d5a53bfad5b042529a1a31e6d
Raul Resendiz <rresendizlazaro@hotmail.com>
2021-03-04 19:30:06 -0600

    Evaluation1

--- src/test/java/mx/edu/j2se/resendiz/evaluation/Circle.java
+++ src/test/java/mx/edu/j2se/resendiz/evaluation/Circle.java
@@ -10,16 +10,28 @@ public class Circle {
     }
 
     public Circle(double radius){
-        if (radius <= 0)
-            throw new IllegalArgumentException();
-        this.radius = radius;
+        try{
+            if (radius <= 0) {
+                throw new IllegalArgumentException();
+            }else{
+                this.radius = radius;
+            }
+        }catch (Exception e){
+            System.out.println("The value is invalid, try another");
+        }
     }
 
     //Methods
     public void setRadius(double radius){
-        if (radius <= 0)
-            throw new IllegalArgumentException();
-        this.radius = radius;
+        try{
+            if (radius <= 0) {
+                throw new IllegalArgumentException();
+            }else{
+                this.radius = radius;
+            }
+        }catch (Exception e){
+            System.out.println("The value is invalid, try another");
+        }
     }
 
     public double getRadius(){
