5776d2a1aaba3c123c09dd276f94674089a53e10
Raul Resendiz <rresendizlazaro@hotmail.com>
2021-03-10 21:51:34 -0600

    TaskListFactory

--- src/main/java/mx/edu/j2se/resendiz/tasks/TaskListFactory.java
+++ src/main/java/mx/edu/j2se/resendiz/tasks/TaskListFactory.java
@@ -1,16 +1,14 @@
 package mx.edu.j2se.resendiz.tasks;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 public class TaskListFactory{
     public static AbstractTaskList createTaskList(ListTypes.types type){
-        if (type.equals(ListTypes.types.ARRAY)){
+        //Returning
+        if (type.equals(ListTypes.types.ARRAY)) {
             return new ArrayTaskList();
-        }
-        if (type.equals(ListTypes.types.LINKED)){
+        }else if (type.equals(ListTypes.types.LINKED)){
             return new LinkedTaskList();
+        }else {
+            return null;
         }
-        return null;
     }
 }
