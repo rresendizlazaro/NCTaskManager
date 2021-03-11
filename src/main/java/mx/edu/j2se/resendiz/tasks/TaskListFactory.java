122933552c06bb299df9b67e8dd6a145fac59c94
Raúl Reséndiz <rresendizlazaro@hotmail.com>
2021-03-10 21:34:55 -0600

    Update TaskListFactory.java

--- src/main/java/mx/edu/j2se/resendiz/tasks/TaskListFactory.java
+++ src/main/java/mx/edu/j2se/resendiz/tasks/TaskListFactory.java
@@ -1,8 +1,5 @@
 package mx.edu.j2se.resendiz.tasks;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 public class TaskListFactory{
     public static AbstractTaskList createTaskList(ListTypes.types type){
         if (type.equals(ListTypes.types.ARRAY)){
